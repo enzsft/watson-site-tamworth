fe15ed3de2d4c4b08c07f99d8f6aa42569b8e22e
Lee Cheneler <lacheneler@gmail.com>
2023-12-30 17:26:37 +0000

    updated pricing

--- pages/index.tsx
+++ pages/index.tsx
@@ -214,9 +214,9 @@ const Services = () => {
               <Text component="dd" m={0}>
                 <Stack spacing="xs">
                   <Text>Routine treatment • £30.00</Text>
-                  <Text>Routine pensioner treatment (65+ years) • £26.00</Text>
-                  <Text>Home visit (1 person) • £33.00*</Text>
-                  <Text>Home visit (2 person) • £55.00*</Text>
+                  <Text>Routine pensioner treatment (65+ years) • £28.00</Text>
+                  <Text>Home visit (1 person) • £35.00*</Text>
+                  <Text>Home visit (2 person) • £58.00*</Text>
                   <Text>Othortics • £40.00+</Text>
                   <Text fw="bold" italic>
                     Missed appointments will incur full fee
